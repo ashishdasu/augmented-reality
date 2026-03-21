c37488f0284b5c75deabf51bf704167a37905cdf
ashishdasu <ash0521@gmail.com>
2026-03-21 15:37:58 -0400

    Scaffolding + video capture loop

---
+++ augmented_reality.cpp
@@ -0,0 +1,20 @@
+#include "utils.h"
+
+int main() {
+    cv::VideoCapture cap(0);
+    if (!cap.isOpened()) {
+        std::cerr << "Error: could not open camera\n";
+        return 1;
+    }
+
+    cv::Mat frame;
+    for (;;) {
+        cap >> frame;
+        if (frame.empty()) continue;
+
+        cv::imshow("AR", frame);
+        if (cv::waitKey(10) == 'q') break;
+    }
+
+    return 0;
+}
---
+++ utils.h
@@ -0,0 +1,6 @@
+#ifndef UTILS_H
+#define UTILS_H
+
+#include <opencv2/opencv.hpp>
+
+#endif // UTILS_H
